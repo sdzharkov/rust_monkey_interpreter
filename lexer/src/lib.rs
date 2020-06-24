768e40dce2ca98941f7d5732d4ff733aba57b037
Stephan Zharkov <stephan.zharkov@gmail.com>
2020-06-23 22:22:44 -0700

    Finalize second test case

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,4 @@
-use token::Token;
+use token::{Token, TokenType};
 
 #[derive(Debug)]
 pub struct Lexer {
@@ -42,16 +42,83 @@ impl Lexer {
         self.read_position += 1;
     }
 
-    pub fn next_token(&mut self) -> Token {
-        let token: Token;
-        if is_letter(&self.ch) {
-            let identifier = self.read_identifier();
-            token = Token::next_identifier_token(identifier);
-        } else {
-            token = Token::next_token(&self.ch);
+    pub fn read_number(&mut self) -> String {
+        let position = self.position;
+
+        while self.ch.is_numeric() {
+            self.read_char();
         }
+
+        self.input[position..self.position].to_string()
+    }
+
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
+        let token: Token = match &self.ch {
+            '=' => Token {
+                token: TokenType::ASSIGN,
+                literal: self.ch.to_string(),
+            },
+            ';' => Token {
+                token: TokenType::SEMICOLON,
+                literal: self.ch.to_string(),
+            },
+            '(' => Token {
+                token: TokenType::LPAREN,
+                literal: self.ch.to_string(),
+            },
+            ')' => Token {
+                token: TokenType::RPAREN,
+                literal: self.ch.to_string(),
+            },
+            ',' => Token {
+                token: TokenType::COMMA,
+                literal: self.ch.to_string(),
+            },
+            '+' => Token {
+                token: TokenType::PLUS,
+                literal: self.ch.to_string(),
+            },
+            '{' => Token {
+                token: TokenType::LBRACE,
+                literal: self.ch.to_string(),
+            },
+            '}' => Token {
+                token: TokenType::RBRACE,
+                literal: self.ch.to_string(),
+            },
+            '0' => Token {
+                token: TokenType::EOF,
+                literal: self.ch.to_string(),
+            },
+            _ => {
+                if is_letter(&self.ch) {
+                    let identifier = self.read_identifier();
+                    Token::lookup_ident(identifier)
+                } else if self.ch.is_numeric() {
+                    let num = self.read_number();
+                    Token {
+                        token: TokenType::INT,
+                        literal: num,
+                    }
+                } else {
+                    Token {
+                        token: TokenType::ILLEGAL,
+                        literal: self.ch.to_string(),
+                    }
+                }
+            }
+        };
+
         self.read_char();
         token
+    }
+
+    pub fn skip_whitespace(&mut self) {
+        while self.ch.is_whitespace() {
+            self.read_char();
+        }
     }
 }
 
--- token/src/lib.rs
+++ token/src/lib.rs
@@ -18,7 +18,6 @@ pub enum TokenType {
     FUNCTION,
     LET,
 }
-  
 #[derive(Debug, PartialEq, Eq)]
 pub struct Token {
     pub token: TokenType,
@@ -26,25 +25,20 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn next_token(character: &char) -> Token {
-        match character {
-            '=' => Token { token: TokenType::ASSIGN, literal: character.to_string() },
-            ';' => Token { token: TokenType::SEMICOLON, literal: character.to_string() },
-            '(' => Token { token: TokenType::LPAREN, literal: character.to_string() },
-            ')' => Token { token: TokenType::RPAREN, literal: character.to_string() },
-            ',' => Token { token: TokenType::COMMA, literal: character.to_string() },
-            '+' => Token { token: TokenType::PLUS, literal: character.to_string() },
-            '{' => Token { token: TokenType::LBRACE, literal: character.to_string() },
-            '}' => Token { token: TokenType::RBRACE, literal: character.to_string() },
-            '0' => Token { token: TokenType::EOF, literal: character.to_string() },
-            _ => Token { token: TokenType::ILLEGAL, literal: character.to_string() }
-        }
-    }
-
-    pub fn next_identifier_token(ident: String) -> Token {
+    pub fn lookup_ident(ident: String) -> Token {
         match ident.as_str() {
-            "let" => Token { token: TokenType::LET, literal: ident },
-            _ => Token { token: TokenType::IDENT(ident.clone()), literal: ident }
+            "let" => Token {
+                token: TokenType::LET,
+                literal: ident,
+            },
+            "fn" => Token {
+                token: TokenType::FUNCTION,
+                literal: ident,
+            },
+            _ => Token {
+                token: TokenType::IDENT(ident.clone()),
+                literal: ident,
+            },
         }
     }
 }
